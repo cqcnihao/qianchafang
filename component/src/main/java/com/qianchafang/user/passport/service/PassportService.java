a3af27a964288446b4820dc71a3c864f3e24f308
pbx <pibicha@163.com>
2017-05-10 11:41:55 +0800

    账户&票据创建，票据验证

---
+++ component/src/main/java/com/qianchafang/user/passport/service/PassportService.java
@@ -0,0 +1,27 @@
+package com.qianchafang.user.passport.service;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * Created by Poan on 2017/5/10.
+ */
+public interface PassportService {
+    /**
+     * 使用微信账户名为其创建账户，密码为其随机设置一个；
+     * 后期再做调整
+     */
+    int createAccount(String wxName);
+
+    /**
+     * 根据用户密码，为其算出一个票据，并存储到db；
+     */
+    String createTicket(int userId);
+
+    /**
+     * 调用本地接口时，需要对其票据进行验证;
+     * 考虑到后期可能会对验证结果进行扩展，不使用boolean，使用integer
+     */
+    Integer verifyTicket(String ticket);
+}
---
+++ component/src/main/java/com/qianchafang/user/passport/service/impl/PassportServiceImpl.java
@@ -0,0 +1,31 @@
+package com.qianchafang.user.passport.service.impl;
+
+import com.qianchafang.user.passport.service.PassportService;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * Created by Poan on 2017/5/10.
+ * 可以通过微信api获取用户名，默认为其创建账户， 后期要做购物的话，
+ * 可以先参考微信提供的接口，再确定是否有必要增加创建账户及密码的环节；
+ */
+public class PassportServiceImpl implements PassportService {
+
+    @Override
+    public int createAccount(String wxName) {
+        return 0;
+    }
+
+
+    @Override
+    public String createTicket(int userId) {
+        return null;
+    }
+
+    @Override
+    public Integer verifyTicket(String ticket) {
+        return null;
+    }
+
+}
